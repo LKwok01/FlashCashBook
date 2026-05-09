7d56116f3a5ed2052bdae8ed00af0bfbc4a83bd2
Lewis <lewiskwokzz@gmail.com>
2026-05-09 09:51:50 +0700

    initial

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FlashCashBook - {{$title}}</title>
+</head>
+<body>
+    we're the forgotten generation, we're the ones who never learn
+</body>
+</html>
