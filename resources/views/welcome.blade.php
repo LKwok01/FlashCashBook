be33fe15bcfab6304ccc21bb417982a048ee40a0
Lewis <lewiskwokzz@gmail.com>
2026-05-11 15:03:34 +0700

    temp view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>FlashCashBook - {{$title}}</title>
-</head>
-<body>
-    we're the forgotten generation, we're the ones who never learn
-</body>
-</html>
+<x-layout>
+    <main class="flex-1 m-1">
+    <div class="">
+    <p class="text-lg text-gray-700">we're the forgotten generation, we want an open conversation</p><br>
+    </div>
+    <div class="p-4" x-data="{ open: false }">
+        <button class="bg-blue-500 text-white py-2 px-4 rounded" @click="open = !open">Toggle</button>
+        <div x-show="open">
+            <p>This is the content that will be toggled.</p>
+        </div>
+
+    </div>
+    <livewire:test-component />
+    </main>
+</x-layout>
