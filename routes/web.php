9c68dd6e888c0f4fa623cdd3396bd7944cb08e2a
Lewis <lewiskwokzz@gmail.com>
2026-05-11 15:04:48 +0700

    temp routes

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     $title = 'Home';
-    return view('welcome', compact('title'));
+    return view('welcome');
+});
+Route::get('/about', function () {
+    $title = 'About Us';
+    return view('welcome',compact('title'));
+});
+Route::get('/contact', function () {
+    $title = 'Contact Us';
+    return view('welcome',compact('title'));
 });
