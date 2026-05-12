f5464c00e1f2ebefdb419dfd6adf9f0bbaecac78
Lewis <lewiskwokzz@gmail.com>
2026-05-12 14:55:30 +0700

    add new section

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ isset($title) ? $title : 'FlashCash' }}</title>
+    <title>{{ isset($title) ? 'FlashCash - ' . $title : 'FlashCash' }}</title>
     @vite(['resources/css/app.css'])
 </head>
 <body class="flex min-h-screen">
@@ -12,13 +12,14 @@
         <h2 class="text-xl font-bold mb-2">FlashCashBook</h2>
         <ul class="w-full">
             <x-nav-link href="/" :active="request()->routeIs('dashboard')">Dashboard</x-nav-link>
-            <x-nav-link href="/daily" :active="request()->routeIs('daily')">Daily Activity</x-nav-link>
+            <x-nav-link href="/daily" :active="request()->routeIs('daily')">Activities</x-nav-link>
+            <x-nav-link href="/daily" :active="request()->routeIs('daily')">Data lookup</x-nav-link>
             <x-nav-link href="/inventory" :active="request()->routeIs('inventory')">Inventory</x-nav-link>
             <x-nav-link href="/cash" :active="request()->routeIs('cash')">Cash Management</x-nav-link>
             <x-nav-link href="/reports" :active="request()->routeIs('reports')">Reports</x-nav-link>
         </ul>
     </aside>
-    <main class="flex-1 m-1">
+    <main class="flex-1">
     {{$slot}}
     </main>
 
