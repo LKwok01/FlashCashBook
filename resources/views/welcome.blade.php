274d61207982eb0e9549440e6b3612bd571b6ebd
Lew <lewiskwokzz@gmail.com>
2026-05-17 16:30:03 +0700

    add catalog management

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,10 @@
 <x-layout>
     <main class="flex-1 m-1">
-    <div class="">
-    <p class="text-lg text-gray-700">we're the forgotten generation, we want an open conversation</p><br>
+        
+    <a href="/catalog">
+        <span class="text-sx font-semibold ">Your Catalog</span>
+    </a>
+    
     </div>
-    <div class="p-4" x-data="{ open: false }">
-        <button class="bg-blue-500 text-white py-2 px-4 rounded" @click="open = !open">Toggle</button>
-        <div x-show="open">
-            <p>This is the content that will be toggled.</p>
-        </div>
-
-    </div>
-    <livewire:test-component />
     </main>
 </x-layout>
